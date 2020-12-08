5cb540bdac203573a41a84b46c5c55858214308f
三 苏 <windy3417@LIVE.COM>
2020-12-08 19:55:58 +0800

    增加文件删除功能

---
+++ Files/FilesOperate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utility.Files
+{
+  public  class FilesOperate
+    {
+        /// <summary>
+        /// 删除文件夹下（包含嵌套文件夹里），所有.exe,.lib,.dll文件
+        /// </summary>
+        /// <param name="file_path">文件路径</param>
+        public void DeleteUserFile(string file_path)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFileSystemEntries(file_path))
+                {
+                    if (Directory.Exists(file))
+                    {
+                        DeleteUserFile(file);
+                    }
+                    else
+                    {
+                        if (File.Exists(file))
+                        {
+                            string extension = System.IO.Path.GetExtension(file);
+                            //if (extension == ".tmp" || extension == ".lib" || extension == ".dll")
+                            if (extension == ".tmp")
+                            {
+                                File.Delete(file);
+                               
+                            }
+                            MessageBox.Show("文件删除成功", "删除提示");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
