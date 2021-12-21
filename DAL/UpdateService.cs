78cdd6c1940d2b05d108be505400e744ea292c2e
luojing <57456236@qq.com>
2021-12-21 15:54:16 +0800

    function added : updating data service

---
+++ DAL/UpdateService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Utility.DAL
+{
+   public  class UpdateService
+    {
+        /// <summary>
+        /// 单表修改
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <typeparam name="TContext"></typeparam>
+        /// <param name="expression"></param>
+        /// <param name="entity"></param>
+        public void Upadate<TEntity,TContext>(Expression<Func<TEntity, bool>> expression, TEntity entity) where TEntity : class 
+                where TContext : DbContext, new ()
+        {
+
+            try
+            {
+                using (var db = new TContext())
+                {
+
+                    TEntity m = db.Set<TEntity>().Where(expression.Compile()).FirstOrDefault<TEntity>();
+
+                    var entity1 = entity.GetType();
+                    var entity2 = m.GetType();
+
+                    var prop = entity1.GetProperties();
+                    var prop2 = entity2.GetProperties();
+
+                    foreach (var item in prop)
+                    {
+                        if (item.GetValue(entity,null) != null)
+                        {
+                            foreach (var item2 in prop2)
+                            {
+                                if (item2.Name == item.Name)
+                                {
+                                    item2.SetValue(m, item.GetValue(entity,null),null);
+                                }
+                            }
+                        }
+                    }
+                    m = entity;
+
+
+
+
+                    db.SaveChanges();
+
+                    MessageBox.Show("数据修改成功");
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message + ex.InnerException);
+            }
+        }
+    }
+}
